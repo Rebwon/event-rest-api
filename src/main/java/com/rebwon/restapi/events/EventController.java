b13385b7f36fb342a60774287c435ef58ad9cd4b
Rebwon <msolo021015@naver.com>
2020-04-27 15:44:44 +0900

    event controller test pass

---
+++ src/main/java/com/rebwon/restapi/events/EventController.java
@@ -0,0 +1,24 @@
+package com.rebwon.restapi.events;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import java.net.URI;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+
+	@PostMapping
+	public ResponseEntity createEvent(@RequestBody Event event) {
+		URI uri = linkTo(EventController.class).slash("/{id}").toUri();
+		event.setId(1);
+		return ResponseEntity.created(uri).body(event);
+	}
+}
--- src/test/java/com/rebwon/restapi/events/EventControllerTests.java
+++ src/test/java/com/rebwon/restapi/events/EventControllerTests.java
@@ -1,7 +1,10 @@
 package com.rebwon.restapi.events;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +13,38 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @WebMvcTest
 public class EventControllerTests {
 
 	@Autowired
 	MockMvc mockMvc;
 
+	@Autowired
+	ObjectMapper objectMapper;
+
 	@Test
 	void createEvent() throws Exception {
+		Event event = Event.builder()
+			.name("Spring")
+			.description("Spring Rest API")
+			.beginEnrollmentDateTime(LocalDateTime.of(2020, 4, 27, 4, 1))
+			.closeEnrollmentDateTime(LocalDateTime.of(2020, 4, 28, 4, 1))
+			.beginEventDateTime(LocalDateTime.of(2020, 4, 30, 1, 1))
+			.endEventDateTime(LocalDateTime.of(2020, 5, 1, 20, 1))
+			.basePrice(100)
+			.maxPrice(200)
+			.limitOfEnrollment(100)
+			.location("강남 D2 스타트 팩토리")
+			.build();
+
 		mockMvc.perform(post("/api/events")
 					.contentType(MediaType.APPLICATION_JSON)
-					.accept(MediaTypes.HAL_JSON))
-			.andExpect(status().isCreated());
+					.accept(MediaTypes.HAL_JSON)
+					.content(objectMapper.writeValueAsString(event)))
+			.andDo(print())
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("id").exists());
 	}
 }
