8bcd825800771e4acc2b47a4b76bd715bd9116da
Rebwon <msolo021015@naver.com>
2020-05-04 12:10:57 +0900

    authorities account test

---
+++ src/test/java/com/rebwon/restapi/accounts/AccountServiceTest.java
@@ -0,0 +1,44 @@
+package com.rebwon.restapi.accounts;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AccountServiceTest {
+
+	@Autowired
+	AccountService accountService;
+
+	@Autowired
+	AccountRepository accountRepository;
+
+	@Test
+	void loadUserByUsername() {
+		// given
+		String username = "rebwon@gmail.com";
+		String password = "rebwon";
+		Account account = Account.builder()
+			.email(username)
+			.password(password)
+			.roles(Set.of(AccountRole.USER, AccountRole.ADMIN))
+			.build();
+		this.accountRepository.save(account);
+
+		// when
+		UserDetailsService userDetailsService = accountService;
+		UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+		// then
+		assertThat(userDetails.getPassword()).isEqualTo(password);
+	}
+}
